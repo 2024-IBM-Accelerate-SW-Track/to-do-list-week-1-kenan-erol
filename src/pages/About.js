d3589470911d9844c69b35e845d542c360e2d354
kenan-erol <kenan.erol@yale.edu>
2024-06-05 21:05:27 -0700

    website is good but todo list does not add.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import bruh from '../assets/profilepic.jpg'
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+			<div className="split left">
+				<div className="centered">
+					<img 
+					className="profile_image"
+					src={bruh} alt="Profile Pic" 
+					></img>
+				</div>
+			</div>
+			<div className="split right">
+				<div className="centered">
+					<div className="name_title">Kenan Erol</div>
+					<div className="brief_description">
+					Hello! I am a software and computer engineer who is interested in hardware applications of artificial intelligence. In my free time, I enjoy reading, hiking, and playing the guitar.
+					</div>
+				</div>
+			</div>
+		</div>
       </div>
     )
   }
